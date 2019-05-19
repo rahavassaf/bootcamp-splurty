b91bc6fe9043ae5de97449b90d9199011ac6f400
Assaf Rahav <rahav.assaf@gmail.com>
2019-05-19 02:30:19 +0000

    moar

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,6 +4,19 @@ class QuotesController < ApplicationController
 	end
 
 	def new
+		@success = params[:success]
 		@quote = Quote.new
+	end
+
+	def create
+		Quote.create(quote_params)
+		redirect_to new_quote_url(:success => 'true')
+	end
+
+	private
+	
+
+	def quote_params
+		params.require(:quote).permit(:saying, :author)
 	end
 end
