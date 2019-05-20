9e7d8d44adfbc01e791c2303b001ea28b2fc43e9
Assaf Rahav <rahav.assaf@gmail.com>
2019-05-20 05:37:01 +0000

    maybe the final?

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,5 @@
 class Quote < ApplicationRecord
 	validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
 	validates :author, presence: true, length: { maximum: 50,  minimum: 3 }
+	validates :youtube_id, format: { with: /\A[0-9a-zA-Z]{0,20}\z/ }
 end
